ac02456cec98a93407b14ad69bd27c097d4ad23e
cmhughes <christopher.michael.hughes@gmail.com>
2014-07-24 22:22:04 +0100

    updated documentation - first fully working version

--- diary.pl
+++ diary.pl
@@ -1,32 +1,80 @@
 #!/usr/bin/perl
+# diary.pl
 #
-# Need to install XML::LibXML which 
-# I did using:
+# PURPOSE:
+#   read in an XML template from a file,
+#   together with START dates and END dates (optional) and append the XML 
+#   with dates in between START and END. It appends the footer file 
+#   to the end. A check is done on the START and END dates, and the 
+#   script will exit if the dates are not valid.
 #
-#   sudo apt-get install libxml-libxml-perl
-# 
-# If this doesn't work, let me know
+# USAGE
+#   diary.pl --start=yyyy-mm-dd --end=yyyy-mm-dd --template=<filename> --footer=<filename>
 #
-# Demonstrations of XML::LibXML
+#   All of the flags are *optional*:
+#       --start=yyyy-mm-dd is the start date, given in ISO format
+#         default value is today's date
+#       --end=yyyy-mm-dd is the end date, given in ISO format
+#         default value is tomorrow's date
+#       --template=<filename> is the name of the template
+#         default value is default: blank-day-template.txt
+#       --footer=<filename> is the name of the footer file
+#         default: other-blank-templates.txt
+#
+# SAMPLE USAGE
+#   You can either run
+#       perl diary.pl
+#   or, if you choose to make the file executable (chmod +x diary.pl), then you can simply run 
+#       diary.pl
+#   You might like to put diary.pl (together with the template files) in /usr/local/bin, or 
+#   otherwise put it somewhere else and then update your PATH variable. 
+#
+#   1. Running
+#           diary.pl
+#      will output to diary-<today's date>-to-<tomorrow's date>.xml
+#   2. Running
+#           diary.pl --start=2014-07-24 --end=2014-09-21
+#      will output to diary-2014-07-24-to-2014-09-21.xml
+#   3. Running
+#           diary.pl --start=2014-02-31
+#      will cause an error, as there are not 31 days in February
+#   4. Running
+#           diary.pl --start=2014-7-24 --end=2014-9-21
+#      will output to diary-2014-07-24-to-2014-09-21.xml
+#      as the script knows to add in the extra 0s
+#
+# REQUIRED PERL modules
+#   Need to install XML::LibXML which 
+#   I did using:
+#
+#     sudo apt-get install libxml-libxml-perl
+#   
+#   If this doesn't work, let me know
+#
+# DEMONSTRATIONS of XML::LibXML
 #   http://www.perlmonks.org/?node_id=490846
 #   http://stackoverflow.com/questions/154762/how-can-i-create-xml-from-perl
 #   http://stackoverflow.com/questions/8411684/xmllibxml-replace-element-value
+# DateTime module reading
+#   http://search.cpan.org/~drolsky/DateTime-1.10/lib/DateTime.pm
 
 # load the perl modules
 use strict;         # strict and warnings should, imho, always be loaded
 use warnings;       # to help find coding mistakes
 use XML::LibXML;    # to process the XML
 use Getopt::Long;   # to get the switches/options/flags
-use DateTime;
+use DateTime;       # for Date and Time manipulations/calculations
 
 # read the arguments
 my %start;
 my %end;
 my $templateFile;
+my $footerFile;
 GetOptions (
   "start=s"=>\$start{input},
   "end=s"=>\$end{input},
   "template=s"=>\$templateFile,
+  "footer=s"=>\$footerFile,
 );
 
 # check the start date, if given
@@ -53,7 +101,7 @@ if(defined $start{input}){
     $start{month} = ($start{month}<10) ? "0".$start{month} : $start{month};
 }
 
-# all the checks have been done, so the $startDate can be formed
+# all the checks have been done, so the $start{print} can be formed
 $start{print} = $start{DayName}." ".$start{year}."-".$start{month}."-".$start{day};
 
 # check the end date, if given
@@ -80,7 +128,7 @@ if(defined $end{input}){
     $end{month} = ($end{month}<10) ? "0".$end{month} : $end{month};
 }
 
-# all the checks have been done, so the $endDate can be formed
+# all the checks have been done, so the $end{print} can be formed
 $end{print} = $end{DayName}." ".$end{year}."-".$end{month}."-".$end{day};
 
 # check to see if dates make sense (start needs to be before end)
@@ -89,15 +137,28 @@ if ($start{date}>$end{date}){
   exit 0;
 }
 
-# XML parsing starts here
-my $filename = (defined $templateFile) ? $templateFile : 'blank-day-template.txt';
-my $parser = XML::LibXML->new();
-my $doc = $parser->parse_file($filename);
+# set up name of output file, and diary tags
+my %diary;
+$diary{tag} = "".$start{year}."-".$start{month}."-".$start{day}."-to-".$end{year}."-".$end{month}."-".$end{day};
 
-print "start = ",$start{print},"\n";
-print "end = ",$end{print},"\n";
-print "start = ",$start{date},"\n";
-print "end = ",$end{date},"\n";
+# open the diary file
+my $diaryfile;
+open($diaryfile,">","diary-$diary{tag}.xml") or die "Can't open $diary{tag}.xml";
+
+# print the header to the diary file
+print $diaryfile <<ENDQUOTE
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="diary.xsl"?>
+
+<diary-$diary{tag}>
+ENDQUOTE
+;
+
+# XML parsing starts here
+$templateFile = (defined $templateFile) ? $templateFile : 'blank-day-template.txt';
+$footerFile = (defined $footerFile) ? $footerFile : 'other-blank-templates.txt';
+my $parser = XML::LibXML->new();
+my $doc = $parser->parse_file($templateFile);
 
 # need a variable for XML replacement
 my $node;
@@ -128,8 +189,32 @@ while($start{date}<=$end{date}){
     $node->removeChildNodes();
     $node->appendText($start{date}->year());
     
-    print $doc->toString();
+    # ugly hack to remove <?xml version.*?> - I'm not proud of it
+    # and hope to change it
+    $tmp = $doc->toString();
+    $tmp =~ s/<\?xml.*\?>//;
+        
+    print $diaryfile $tmp;
     $start{date}->add(days=>1);
 }
 
+# close the diary tag
+print $diaryfile <<ENDQUOTE
+
+</diary-$diary{tag}>
+
+<!--
+ENDQUOTE
+;
+
+# print the 'footer' to the diary file
+open(FOOTER, $footerFile) or die "Could not open $footerFile";
+while(<FOOTER>){
+    print $diaryfile $_;
+}
+close(FOOTER);
+
+# last comment
+print $diaryfile "-->";
+close($diaryfile);
 exit;
